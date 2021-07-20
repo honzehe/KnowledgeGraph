f98e917857560ad01e8c67c6fbe621beb265ba10
zehe <1522309245@qq.com>
2021-07-20 19:17:37 +0800

    第一版

---
+++ src/main/java/com/knowledge/graph/service/BIMService.java
@@ -0,0 +1,64 @@
+package com.knowledge.graph.service;
+
+import com.knowledge.graph.mapper.BIMMapper;
+import com.knowledge.graph.mapper.RelationshipMapper;
+import com.knowledge.graph.node.BIMNode;
+import com.knowledge.graph.node.EndNodeWithRelationship;
+import com.knowledge.graph.node.RelationshipNode;
+import com.knowledge.graph.node.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class BIMService {
+
+    @Autowired
+    private BIMMapper bimMapper;
+
+    @Autowired
+    private RelationshipMapper relationshipMapper;
+
+    public Response getAllNode(){
+        List<BIMNode> bimNodes =  bimMapper.getAllNode();
+        HashMap hashMap = new HashMap();
+        hashMap.put("nodes",bimNodes);
+        return Response.Ok("查询所有结点成功",hashMap);
+    }
+
+    public Response getAllRelationship(){
+        List<RelationshipNode> relationships = relationshipMapper.getAllRelationship();
+        HashMap hashMap = new HashMap();
+        hashMap.put("relationships",relationships);
+        return Response.Ok("查询所有联系成功",hashMap);
+    }
+    public Map<String, Object> getProperties(Long id){
+        return bimMapper.getPropertiesById(id);
+    }
+
+    public Response getNodeByName(String name){
+        List<BIMNode> bimNodes = bimMapper.getNodeByName(name);
+        HashMap hashMap = new HashMap();
+        hashMap.put("nodes",bimNodes);
+        return Response.Ok("模糊查询结点成功",hashMap);
+    }
+
+    public Response getEndNodeWithRelationshipById(Long id){
+        EndNodeWithRelationship endNodes = new EndNodeWithRelationship();
+        endNodes.setEndNodes(bimMapper.getNodesByStartId(id));
+        endNodes.setRelationshipNodes(relationshipMapper.getRelationshipByStartId(id));
+        HashMap hashMap = new HashMap();
+        hashMap.put("endNodesWithRelationship",endNodes);
+        return Response.Ok("查询某结点以及其联系成功", hashMap);
+    }
+
+    public Response getNodeById(Long id){
+        BIMNode node = bimMapper.getBIMById(id);
+        HashMap hashMap = new HashMap();
+        hashMap.put("node",node);
+        return Response.Ok("根据结点id查找结点成功", hashMap);
+    }
+}
